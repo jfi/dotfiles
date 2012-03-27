be4eb5eee83aa36eceeefdf89ec458634986d24c
James Inman <james@jamesinman.co.uk>
2012-03-27 08:56:08 +0100

    Adding auto-installer.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Various configuration files:
 - .gitconfig
 - .gitignore
 - .tm_properties
+
+Run `ruby install.rb` to copy the files to the correct locations.
---
+++ install.rb
@@ -0,0 +1,12 @@
+`cp -f zshrc ~/.zshrc`
+puts ".zshrc copied to ~/.zshrc"
+`cp -f gitignore ~/.gitignore`
+puts ".gitignore copied to ~/.gitignore"
+`cp -f gitconfig ~/.gitconfig`
+puts ".gitconfig copied to ~/.gitconfig"
+`cp -f robbyrussell.zsh-theme ~/.oh-my-zsh/themes/robbyrussell.zsh-theme`
+puts "robbyrussell.zsh-theme copied to ~/.oh-my-zsh/themes/robbyrussell.zsh-theme"
+`cp -f tm_properties ~/.tm_properties`
+puts ".tm_properties copied to ~/.tm_properties"
+
+puts "--- COMPLETE ---"
